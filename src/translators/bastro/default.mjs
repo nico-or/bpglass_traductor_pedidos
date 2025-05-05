27f70f3b14364a7f2b77ddab980ecded032fb966
nicolas <nicordenes@gmail.comm>
2025-05-04 20:51:09 -0400

    refactor(translators/bastro/default): make logic more consistent
    The output data was too inconsistent, sometimes outputing empty string, and 0 in others.
    We decide to return undefined as a default for all fields.
    
    Also some minor rewriting of the FORMA field parsing.

--- src/translators/bastro/default.mjs
+++ src/translators/bastro/default.mjs
@@ -21,17 +21,22 @@ export default function translate(inputText) {
   const pedidos = lines.map((arr) => {
     arr[3] = normalizeString(arr[3]);
 
+    // Check if the ancho field contains any non-numeric characters to determine if it's a form
+    const isForma = arr[3].replace(/[0-9]/g, "") !== "";
+
     return {
-      referencia: arr[3] === "FORMA" ? `FORMA ${arr[1]}` : arr[1],
+      referencia: isForma ? `FORMA ${arr[1]}` : arr[1],
       cantidad: excelString2Number(arr[2]),
-      ancho: arr[3] === "FORMA" ? 0 : excelString2Number(arr[3]),
-      alto: excelString2Number(arr[4]),
+      ancho: isForma ? undefined : excelString2Number(arr[3]),
+      alto: isForma ? undefined : excelString2Number(arr[4]),
       composicion: {
         vidrio_1: findVidrio(arr[5]),
         vidrio_2: findVidrio(arr[6]),
+        vidrio_3: undefined,
         separador_1: findSeparador(arr[7]),
+        separador_2: undefined,
       },
-      forma: arr[3] === "FORMA",
+      forma: isForma,
     };
   });
 
