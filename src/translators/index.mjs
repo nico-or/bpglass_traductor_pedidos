f3f753872d867d68f9618a2965b3dacd8762f780
nicolas <nicordenes@gmail.comm>
2025-05-06 12:22:22 -0400

    feat: delete unused translators

--- src/translators/index.mjs
+++ src/translators/index.mjs
@@ -1,10 +1,8 @@
 import bastro from "./bastro/default.mjs";
 import naranjo from "./renovatek/naranjo.mjs";
-import azul from "./renovatek/azul.mjs";
 
 // The keys need to match the value of the mode_selector in the HTML file.
 export const translators = {
   bst: bastro,
   rtk_nar: naranjo,
-  rtk_azu: azul,
 };
